f43dd785271df2b412ea742067499a53bff86637
Nikita <68944906+BANanaD3V@users.noreply.github.com>
2025-04-13 12:47:11 +0300

    Packages: use with

--- modules/nixos/lucy-packages.nix
+++ modules/nixos/lucy-packages.nix
@@ -1,27 +1,30 @@
-{ lib, config, pkgs, ... }: let
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
   cfg = config.lucy-packages;
 in {
-  # Voxi0 -> I'll figure out how to simplify all this and move it somewhere more appropriate
-
   # Module options
   options.lucy-packages.enable = lib.mkEnableOption "Enable Lucy's Packages";
 
   # Configuration - Remove some Gnome packages
   config = lib.mkIf cfg.enable {
     # Remove some Gnome packages
-    environment.gnome.excludePackages = with pkgs.gnome; [
+    environment.gnome.excludePackages = with pkgs; [
       # gedit             # Text editor
       # file-roller       # Archive manager
-      pkgs.baobab         # Disk usage analyzer
-      pkgs.cheese         # Photo booth
-      pkgs.eog            # Image viewer
-      pkgs.epiphany       # Web browser
-      pkgs.simple-scan    # Document scanner
-      pkgs.totem          # Video player
-      pkgs.yelp           # Help viewer
-      pkgs.evince         # Document viewer
-      pkgs.geary          # Email client
-      pkgs.seahorse       # Password manager
+      baobab # Disk usage analyzer
+      cheese # Photo booth
+      eog # Image viewer
+      epiphany # Web browser
+      simple-scan # Document scanner
+      totem # Video player
+      yelp # Help viewer
+      evince # Document viewer
+      geary # Email client
+      seahorse # Password manager
 
       # These should be self explanatory
       # gnome-calculator
@@ -30,15 +33,15 @@ in {
       # gnome-clocks
       # gnome-font-viewer
       # gnome-screenshot
-      pkgs.gnome-contacts
-      pkgs.gnome-logs
-      pkgs.gnome-maps
-      pkgs.gnome-music
-      pkgs.gnome-photos
-      pkgs.gnome-system-monitor
-      pkgs.gnome-weather
-      pkgs.gnome-disk-utility
-      pkgs.gnome-connections
+      gnome-contacts
+      gnome-logs
+      gnome-maps
+      gnome-music
+      gnome-photos
+      gnome-system-monitor
+      gnome-weather
+      gnome-disk-utility
+      gnome-connections
     ];
   };
 }
