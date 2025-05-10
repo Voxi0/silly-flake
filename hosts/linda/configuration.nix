ff3d8dc5c3eddee556794882b9ff413175e5734a
Voxi0 <alif200099@gmail.com>
2025-05-10 13:24:00 +0100

    Fixed Stoobid Bug
    im stoobuid

--- hosts/linda/configuration.nix
+++ hosts/linda/configuration.nix
@@ -11,15 +11,13 @@
   nixpkgs.config.allowUnfree = true;
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
-    settings = {
-      substituters = [
-        "https://nix-community.cachix.org"
-        "https://cache.nixos.org/"
-      ];
-      trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
-    };
+    substituters = [
+      "https://nix-community.cachix.org"
+      "https://cache.nixos.org/"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   # Virtual console and boot
