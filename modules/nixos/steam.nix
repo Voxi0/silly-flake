8bc0ff84b58828716ffad6c7947d18d9cf3b8422
voxi0 <alif200099@gmail.com>
2025-07-24 21:05:35 +0100

    Light Cleaning
    Barely cleaned anything really.

--- modules/nixos/steam.nix
+++ modules/nixos/steam.nix
@@ -4,8 +4,6 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
+    extraCompatPackages = [pkgs.proton-ge-bin];
   };
 }
